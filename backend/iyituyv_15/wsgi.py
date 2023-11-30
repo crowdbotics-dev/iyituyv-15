8e251d2394d17a78a512c053522ef6a986fe57df
Crowdbotics <engineering@crowdbotics.com>
2023-11-30 09:59:47 +0000

    Initial commit

---
+++ backend/iyituyv_15/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for iyituyv_15 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "iyituyv_15.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "iyituyv-15",
+  url: "https://iyituyv-15.botics.co",
+  api: "https://iyituyv-15.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
